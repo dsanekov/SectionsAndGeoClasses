b25b8c11a53143fddcb99c3f1bf6d62fbaad1fe0
Daniil Anekov <daniilanekov@mail.ru>
2023-03-08 22:05:08 +0300

    Initial commit

---
+++ src/main/java/ru/natlex/natlexTestApp/NatlexTestAppApplication.java
@@ -0,0 +1,13 @@
+package ru.natlex.natlexTestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatlexTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatlexTestAppApplication.class, args);
+	}
+
+}
